00ba902e1c2c968198f8853f13487ead841e4362
Geoffroy Aubry <geoffroy.aubry@twenga.com>
2011-05-26 12:12:41 +0200

    POC WS Redmine
    Change-Id: I1c182520f0437ad148eb060607e55c51b0e8dc8c

---
+++ inc/ws_redmine.inc.php
@@ -0,0 +1,42 @@
+<?php
+
+// /usr/bin/php -q ~/twgit/inc/ws_redmine.inc.php 7573
+// http://localhost/TwGit/inc/ws_redmine.inc.php?issue=7573
+
+// http://redmine.twenga.com/projects/api2/issues.xml
+
+include_once(__DIR__ . '/../lib/phpactiveresource/ActiveResource.php');
+define('USERNAME', 'gaubry');
+define('PASSWORD', 'jR7nN0');
+define('SITE_URL', 'http://' . USERNAME . ':' . PASSWORD . '@redmine.twenga.com/');
+
+class Issue extends ActiveResource {
+    var $site = SITE_URL;
+    var $request_format = 'xml'; // REQUIRED!
+    var $extra_params = '?key=c0bb67bc6ae9b1693ac3f06a8e7fd8f5eefa425f';
+}
+
+if (isset($_GET['issue'])) {
+	$issue_id = $_GET['issue'];
+} else {
+	$argc--;
+	array_shift($argv);
+	if ($argc < 1) {
+		throw new Exception('Issue ID missing!');
+	}
+	$issue_id = $argv[0];
+}
+
+$issue = new Issue (array ('subject' => 'XML REST API'));
+$issue->find($issue_id);
+if ($issue->error !== false) {
+	file_put_contents('php://stderr', $issue->error . "\n" . $issue->response_headers, E_USER_ERROR);
+} else if (strcasecmp($issue_id, $issue->id) !== 0) {
+	file_put_contents('php://stderr', "Requested ID: '$issue_id'. ID found: '" . $issue->id . "'", E_USER_ERROR);
+} else {
+	echo 'ID: ' . $issue->id . "\n";
+	echo 'Parent: ' . $issue->parent . "\n";
+	echo 'Project: ' . $issue->project . "\n";
+	echo 'Subject: ' . $issue->subject . "\n";
+	echo 'Description: ' . $issue->description . "\n";
+}

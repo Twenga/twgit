ac189cf479e58e946dc1bd6126f57b48fecd91d7
Geoffroy Aubry <gaubry@hi-media.com>
2012-01-11 17:21:21 +0100

    ADD license

--- inc/twgit_release.inc.sh
+++ inc/twgit_release.inc.sh
@@ -1,5 +1,18 @@
 #!/bin/bash
 
+##
+# twgit
+#
+# Copyright (c) 2011 Twenga SA.
+#
+# This work is licensed under the Creative Commons Attribution-NonCommercial-ShareAlike 3.0 Unported License.
+# To view a copy of this license, visit http://creativecommons.org/licenses/by-nc-sa/3.0/
+# or send a letter to Creative Commons, 444 Castro Street, Suite 900, Mountain View, California, 94041, USA.
+#
+# @copyright 2011 Twenga SA
+# @license http://creativecommons.org/licenses/by-nc-sa/3.0/
+#
+
 assert_git_repository
 assert_php_curl
 
